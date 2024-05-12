7c322e63b1036ec58920a14d7707e13106263349
CatalinChira <catalin_chira05@yahoo.com>
2024-05-12 22:21:41 +0300

    initial commit

---
+++ src/main/java/RandomPhoneNumberSeletion/RandomPhoneNumberSeletion/RandomPhoneNumberSeletionApplication.java
@@ -0,0 +1,13 @@
+package RandomPhoneNumberSeletion.RandomPhoneNumberSeletion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomPhoneNumberSeletionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomPhoneNumberSeletionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RandomPhoneNumberSeletion/RandomPhoneNumberSeletion/RandomPhoneNumberSeletionApplicationTests.java
@@ -0,0 +1,13 @@
+package RandomPhoneNumberSeletion.RandomPhoneNumberSeletion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomPhoneNumberSeletionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
